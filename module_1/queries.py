ce4378aa9b99e3a3fb8822e868dd1cbf82df8123
422christopher <422christopher@gmail.com>
2022-12-19 09:26:07 -0700

    add a bigger query for avg item weight

--- module_1/queries.py
+++ module_1/queries.py
@@ -1,1 +1,10 @@
-SELECT_ALL = 'SELECT character_id, name FROM charactercreator_character;' # end with ;
+SELECT_ALL = 'SELECT character_id, name FROM charactercreator_character;'  # end with ;
+
+
+AVG_ITEM_WEIGHT_PER_CHAR = '''
+    SELECT cc_char.name, AVG(ai.weight) AS avg_item_weight FROM charactercreator_character AS cc_char
+    JOIN charactercreator_character_inventory AS cc_inv
+    ON cc_char.character_id = cc_inv.character_id
+    JOIN armory_item AS ai
+    ON cc_inv.item_id = ai.item_id
+    GROUP BY cc_char.character_id'''
--- module_1/sqlite_example.py
+++ module_1/sqlite_example.py
@@ -1,12 +1,35 @@
-# Step 0: Import sqlite
 import sqlite3
 import queries as q
+import pandas as pd
+
+# Create a DB function
+def connect_to_db(db_name='rpg_db.sqlite3'):
+    return sqlite3.connect(db_name)
+
+def execute_q(conn, query):
+    # create the cursor
+    curs = conn.cursor()
+
+    # use the cursor to execute a particular query
+    curs.execute(query)
+
+    return curs.fetchall()
+
+if __name__ == '__main__':
+    # create a connection to the database
+    conn = connect_to_db()
+    # execute a specific query
+    # print(execute_q(conn, q.SELECT_ALL)[:5])
+    results = execute_q(conn, q.AVG_ITEM_WEIGHT_PER_CHAR)
+    df = pd.DataFrame(results)
+    print(df.head())
+
 
 # step 1: connect to database
 # triple-check spelling because if the database
 # does not exist, then sqlite3 will create a new database
 # with that misspelled name
-connection = sqlite3.connect('rpg_db.sqlite3')
+# connection = sqlite3.connect('rpg_db.sqlite3')
 
 # step 2: create a cursor
 # cursor is like a bank teller who goes to the vault to get your withdrawal
@@ -15,7 +38,7 @@ connection = sqlite3.connect('rpg_db.sqlite3')
 # to access the money (or in this case, data) in a safe, appropriate manner.
 # So we can never directly touch or query the database, it must be done
 # through a cursor.
-cursor = connection.cursor()
+# cursor = connection.cursor()
 
 # step 3: create the query
 # but it might be nice to have all queries in their own file
@@ -23,8 +46,4 @@ cursor = connection.cursor()
 
 # step 4: execute the query and fetch the results (grab the money)
 # but don't repeat yourself
-results = cursor.execute(q.SELECT_ALL).fetchall()
-
-
-if __name__ == '__main__':
-    print(results[:5])
+# results = cursor.execute(q.SELECT_ALL).fetchall()
